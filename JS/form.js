92903cc191a21d862edf60fa9f43163f87ad14c8
TanmayGadge <tanmaygadge9273@gmail.com>
2021-01-25 19:51:22 +0530

    Wait state for the car race game

---
+++ JS/form.js
@@ -0,0 +1,25 @@
+class Form{
+    constructor(){
+
+    }
+    display(){
+        var title = createElement('h2');
+        title.html("Asphalt");
+        title.position(130, 0);
+        var input = createInput("Enter Name: ");
+        input.position(130, 160);
+        var button = createButton("Enter");
+        button.position(250, 200);
+        var greeting = createElement('h3');
+        button.mousePressed(function(){
+            input.hide();
+            button.hide();
+            var name = input.value();
+            playerCount += 1;
+            player.update(name);
+            player.updateCount(playerCount);
+            greeting.html("Hello "+ name + ", welcome to my game.<br>Please wait for others to join.");
+            greeting.position(130, 160);
+        })
+    }
+}
